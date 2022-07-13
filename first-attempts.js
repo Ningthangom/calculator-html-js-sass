d290e693198b9271e78cd0fca6b8886c4e929ab9
Ningthangom <ningthangom@gmail.com>
2022-07-13 10:02:49 +0930

    refactor the codes

---
+++ first-attempts.js
@@ -0,0 +1,82 @@
+/* function writeANumber (number) {
+    switch (number) {
+        case 1:
+            inputString = inputString.concat("1");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case 2:
+            inputString = inputString.concat("2");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case 3:
+            inputString = inputString.concat("3");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case 4:
+            inputString = inputString.concat("4");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case 5:
+            inputString = inputString.concat("5");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case 6:
+            inputString = inputString.concat("6");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case 7:
+            inputString = inputString.concat("7");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case 8:
+            inputString = inputString.concat("8");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case 9:
+            inputString = inputString.concat("9");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case 0:
+            inputString = inputString.concat("0");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        default:
+            return 'enter a number';
+    }
+    
+} */
+
+
+
+
+// click Math operation
+
+/* function mathOperations (operator) {
+    switch (operator) {
+        case "+":
+            inputString = checkExistingOperator(inputString, "+");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case "-":
+            inputString = checkExistingOperator(inputString, "-");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case "%":
+            inputString = checkExistingOperator(inputString, "%");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case "÷":
+            inputString = checkExistingOperator(inputString, "÷");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        case "x":
+            inputString = checkExistingOperator(inputString, "x");
+            document.getElementById("screen").innerHTML = `${inputString}`;
+            break;
+        default:
+            return 'enter a proper math sign';
+    }
+}
+ */
+
+
--- functionalities.js
+++ functionalities.js
@@ -1,19 +1,39 @@
 
 
-// bind click handler to element that is added later/dynamically
-document.addEventListener('click', function(e){
-    if(e.target && e.target.id == '1'){
-         //do something
-         console.log("1")
-    }
+let inputString = "";
+
+// get the operators 
+const inputOperator = document.querySelectorAll(".operator");
+
+inputOperator.forEach((button) => {
+    button.addEventListener("click", () => getOperators(button.value));
 });
 
-const fistArray = [];
-let inputString = "";
-let firstNumber; 
-let joinedfistArray;
+function getOperators(operator) {
+    /*  inputString = inputString.concat(operator); */
+    if (operator == "c") {
+        console.log("delete button is called: ")
+        inputString = "";
+        document.getElementById("screen").innerHTML = `${inputString}`;
+    } else {
+        inputString = checkExistingOperator(inputString, operator);
+        document.getElementById("screen").innerHTML = `${inputString}`;
+    }
+}
 
-const secondArray = [];
+
+// get numbers
+const inputNumber = document.querySelectorAll(".number");
+function getValues(input) {
+    inputString = inputString.concat(input);
+    console.log(inputString);
+    document.getElementById("screen").innerHTML = `${inputString}`;
+}
+
+inputNumber.forEach((button) => {
+    button.addEventListener("click", () => getValues(button.value));
+});
+
 
 function calculation (firstNumber, secondNumber , operation) {
 
@@ -26,7 +46,7 @@ function calculation (firstNumber, secondNumber , operation) {
     }else if (operation == "x") {
         return firstNumber * secondNumber;
     }else if (operation == '%') {
-        return firstNumber % secondNumber;
+        return (firstNumber  ) * secondNumber;
     }
 }
 
@@ -36,19 +56,15 @@ function checkExistingOperator (string,enteredOperator){
     console.log("this is the first index of the string: ", typeof string[0], string[0])
     if (string[0] ==='-'){
         console.log("this is the whole string before removing minus: ", string);
-        // convert string to an array 
-        /* let stringToArray = [...string];
-        console.log("string to array: ", stringToArray); */
-
-        
+  
         let stringNoMinus = string.substring(1);
         console.log("this is String without minus: ", stringNoMinus)
 
         // Array to s
-         if (string.charAt(string.search(/[+\-\*\/\÷\x\%]/))) {
+         if (stringNoMinus.charAt(stringNoMinus.search(/[+\-\*\/\÷\x\%]/))) {
              //split the string values
              const values = stringNoMinus.split(/[+\-\*\/\÷\x\%]/);
-             console.log("checking nagative value", values);
+             console.log("checking negative value", values);
              // to do: need to figure out how to handle negative value
              console.log(
                  "this is negative number: ",
@@ -61,7 +77,7 @@ function checkExistingOperator (string,enteredOperator){
 
              //call calculation function to do math operation on the first two
              const calResult = calculation(
-                 //adding nagative value back since it was removed
+                 //adding negative value back since it was removed
                  parseFloat(`-${values[0]}`),
                  parseFloat(values[1]),
                  operators,
@@ -85,11 +101,11 @@ function checkExistingOperator (string,enteredOperator){
              console.log("string when no operator: ", string);
              return string;
          }
-    } else {
+    } else  {
          if (string.charAt(string.search(/[+\-\*\/\÷\x\%]/))) {
              //split the string values
              const values = string.split(/[+\-\*\/\÷\x\%]/);
-             console.log("checking nagative value", values);
+             console.log("checking negative value", values);
              // to do: need to figure out how to handle negative value
 
              //get the operator
@@ -121,139 +137,10 @@ function checkExistingOperator (string,enteredOperator){
              return string;
          }
 
-    }
-    
-    
+        }
 
 }
 
 
-function writeANumber (number) {
-    switch (number) {
-        case 1:
-            inputString = inputString.concat("1");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case 2:
-            inputString = inputString.concat("2");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case 3:
-            inputString = inputString.concat("3");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case 4:
-            inputString = inputString.concat("4");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case 5:
-            inputString = inputString.concat("5");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case 6:
-            inputString = inputString.concat("6");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case 7:
-            inputString = inputString.concat("7");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case 8:
-            inputString = inputString.concat("8");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case 9:
-            inputString = inputString.concat("9");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case 0:
-            inputString = inputString.concat("0");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        default:
-            return 'enter a number';
-    }
-    
-}
-
-// click Math operation
-
-function mathOperations (operator) {
-    switch (operator) {
-        case "+":
-            inputString = checkExistingOperator(inputString, "+");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case "-":
-            inputString = checkExistingOperator(inputString, "-");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case "%":
-            inputString = checkExistingOperator(inputString, "%");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case "÷":
-            inputString = checkExistingOperator(inputString, "÷");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        case "x":
-            inputString = checkExistingOperator(inputString, "x");
-            document.getElementById("screen").innerHTML = `${inputString}`;
-            break;
-        default:
-            return 'enter a proper math sign';
-    }
-}
 
 
-// delete number from calculator 
-function deleteArray(id) {
-    var el = document.getElementById(id);
-    /*  el.style.color = "blue"; */
-    console.log("one");
-    fistArray.length = 0;
-     console.log(fistArray);
-     const joinedfistArray = fistArray.join("");
-     inputString = "";
-    document.getElementById("screen").innerHTML = `${inputString}`;
-}
-
-
-// 
-
-
-// when press add or minus or multiply
-// check the string if it has one of the signs
-// if there is, split the number in two string
-// convert those strings into float 
-// then based on the math sign perform a calculation
-// then return the result 
-// othewise return the value of entered value
-
-
-// how do i decide to put it in the first or second or third array;
-
-// put all the numbers in one array including the + and - and %
-// when press equal 
-// and take all the numbers from the front of + and take all the numbers after +
-// then convert them into numbers and then add them 
-
-
-// when click equal 
-// sort the array = split the array based on a string 
-// do math operation of the first two array based on the first string
-    // join the numbers from the first two arrays and parseFloat to convert from string to number
-    // perform math operation based on the first string 
-    // save the result back into the array as the first index of the array 
-    // 
-// print the result out save it as a const 
-// delete the first array and add the result to the first array again
-
-
-// numbers before the math operation that
-// numbers after the math operation 
-// numbers after the equal sign 
-
-
-
-// 
