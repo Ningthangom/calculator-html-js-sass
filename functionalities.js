888c44908044e79b99b70c0fbaeb8cec33de97b6
Ningthangom <ningthangom@gmail.com>
2022-07-13 13:44:43 +0930

    move codes to files

--- functionalities.js
+++ functionalities.js
@@ -3,22 +3,6 @@ import {checkExistingOperator} from './checkOperator.js';
 import { calculation } from "./calculation.js";
 
 let inputString = "";
-let calOn = false;
- function toggle() {
-     calOn = calOn ? false : true;
-       console.log("Toggled bool is", calOn);
-           
-   if (calOn == false) {
-         document.getElementById("onOff").innerHTML = `Off`;
-         const btn = document.getElementsByClassName("number");
-         btn.setAttribute("disabled", "");
-   } else {
-       /* document.getElementsByClassName("number").disabled = false; */
-        document.getElementById("onOff").innerHTML = `On`;
-        const btn = document.getElementsByClassName("number");
-        btn.removeAttribute("disabled");
-   }
- }
 
 
 // get the operators 
---
+++ onOff.js
@@ -0,0 +1,16 @@
+/* let calOn = false;
+ function toggle() {
+     calOn = calOn ? false : true;
+       console.log("Toggled bool is", calOn);
+           
+   if (calOn == false) {
+         document.getElementById("onOff").innerHTML = `Off`;
+         const btn = document.getElementsByClassName("number");
+         btn.setAttribute("disabled", "");
+   } else {
+       // document.getElementsByClassName("number").disabled = false; 
+        document.getElementById("onOff").innerHTML = `On`;
+        const btn = document.getElementsByClassName("number");
+        btn.removeAttribute("disabled");
+   }
+ } */
